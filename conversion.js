4307977ce99e9a19d012f6c12dacc6ab8d929687
marcopollacci <pollacci@gmail.com>
2020-11-06 12:57:01 +0100

    better code

--- conversion.js
+++ conversion.js
@@ -9,17 +9,8 @@ module.exports = {
   makeCalc: async function(r, g, b) {
     var max = Math.max(r, g, b), min = Math.min(r, g, b),
         d = max - min,
-        h,
         s = (max === 0 ? 0 : d / max),
         v = max / 255;
-
-        switch (max) {
-          case min: h = 0; break;
-          case r: h = (g - b) + d * (g < b ? 6: 0); h /= 6 * d; break;
-          case g: h = (b - r) + d * 2; h /= 6 * d; break;
-          case b: h = (r - g) + d * 4; h /= 6 * d; break;
-
-      }
 
     return {
         h: await this.choose(max, min, d, Number(r), Number(g), Number(b)),
@@ -42,7 +33,8 @@ module.exports = {
   },
   DecimalHexTwosComplement: async function(decimal) {
       var size = 4;
-    
+      let i;
+
       if (decimal >= 0) {
         var hexadecimal = decimal.toString(16);
     
